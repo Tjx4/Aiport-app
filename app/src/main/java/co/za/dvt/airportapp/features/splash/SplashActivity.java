3fb7eafaca6601ba9a2d7e85e4eefe98423d1c19
Tshepo <rocboyt@gmail.com>
2019-06-12 15:29:08 +0200

    Added navigation to dashboard

--- app/src/main/java/co/za/dvt/airportapp/features/splash/SplashActivity.java
+++ app/src/main/java/co/za/dvt/airportapp/features/splash/SplashActivity.java
@@ -2,6 +2,7 @@ package co.za.dvt.airportapp.features.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import co.za.dvt.airportapp.features.dashboard.DashboardActivity;
 import co.za.dvt.airportapp.helpers.NavigationHelper;
 import co.za.dvt.airportapp.helpers.TransitionHelper;
 
@@ -9,5 +10,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        NavigationHelper.goToActivityWithNoPayload(this, DashboardActivity.class, TransitionHelper.fadeInActivity());
     }
 }
