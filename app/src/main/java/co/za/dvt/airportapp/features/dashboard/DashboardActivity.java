d7faa193c2d8fdb762934ed2851889f8490a69da
Tshepo <rocboyt@gmail.com>
2019-06-12 15:34:11 +0200

    Added dashboard activity

---
+++ app/src/main/java/co/za/dvt/airportapp/features/dashboard/DashboardActivity.java
@@ -0,0 +1,14 @@
+package co.za.dvt.airportapp.features.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import co.za.dvt.airportapp.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_activity);
+    }
+}
