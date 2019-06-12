69ea542ca54e9e399c07b320e445dc62b24b12d5
Tshepo <rocboyt@gmail.com>
2019-06-12 20:26:09 +0200

    Added airportModel

---
+++ app/src/main/java/co/za/dvt/airportapp/models/AirportModel.java
@@ -0,0 +1,31 @@
+package co.za.dvt.airportapp.models;
+
+public class AirportModel {
+    private String iataCode;
+    private double latitude;
+    private double longitude;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getIataCode() {
+        return iataCode;
+    }
+
+    public void setIataCode(String iataCode) {
+        this.iataCode = iataCode;
+    }
+}
