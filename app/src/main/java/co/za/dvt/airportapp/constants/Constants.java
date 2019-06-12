6897f42abdbbd36063d1a92fa5d72915d6a7e9d9
Tshepo <rocboyt@gmail.com>
2019-06-12 15:19:30 +0200

    Added navigation helper

---
+++ app/src/main/java/co/za/dvt/airportapp/constants/Constants.java
@@ -0,0 +1,6 @@
+package co.za.dvt.airportapp.constants;
+
+public class Constants {
+    public static final String PAYLOAD_KEY = "payload";
+    public static final String ACTIVITY_TRANSITION = "acticity_transition";
+}
---
+++ app/src/main/java/co/za/dvt/airportapp/helpers/NavigationHelper.java
@@ -0,0 +1,29 @@
+package co.za.dvt.airportapp.helpers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import co.za.dvt.airportapp.constants.Constants;
+
+public class NavigationHelper {
+
+    private static void goToActivity(Context activityFrom, Class activityTo, int[] transitionAnimation, Bundle payload) {
+        Intent intent = new Intent(activityFrom, activityTo);
+
+        Bundle fullPayload = (payload == null) ? new Bundle() : payload;
+        fullPayload.putIntArray(Constants.ACTIVITY_TRANSITION, transitionAnimation);
+
+        intent.putExtra(Constants.PAYLOAD_KEY, fullPayload);
+        activityFrom.startActivity(intent);
+    }
+
+    public static void goToActivityWithNoPayload(Context activityFrom, Class activity, int[]  transitionAnimation) {
+        goToActivity(activityFrom, activity, transitionAnimation, null);
+    }
+
+    public static void goToActivityWithPayload(Context activityFrom, Class activity, Bundle payload, int[]  transitionAnimation) {
+        goToActivity(activityFrom, activity, transitionAnimation, payload);
+    }
+
+}
