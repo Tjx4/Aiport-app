6f825277959f7e13bba93d0eb20e02f2aa182757
Tshepo <rocboyt@gmail.com>
2019-06-15 18:56:05 +0200

    Added api call

--- app/src/main/java/co/za/dvt/airportapp/helpers/RetrofitHelper.java
+++ app/src/main/java/co/za/dvt/airportapp/helpers/RetrofitHelper.java
@@ -2,14 +2,13 @@ package co.za.dvt.airportapp.helpers;
 
 import java.util.Map;
 import co.za.dvt.airportapp.constants.Constants;
-import co.za.dvt.airportapp.models.AirportsModel;
+import co.za.dvt.airportapp.models.NearbyAirportsModel;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
 
 public interface RetrofitHelper {
-
-    @GET(Constants.GET_AIRPORTS)
-    Call<AirportsModel> getNearbyAirports(@Path("api_key") String api_key, @QueryMap Map<String, String> params);
+    @GET(Constants.NEARBY_AIRPORTS)
+    Call<NearbyAirportsModel> getNearbyAirports(@Query("api_key") String api_key, @QueryMap Map<String, String> params);
 }
