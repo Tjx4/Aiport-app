d5000a7541de18bc13917f06101a97d1ad0fc01c
Tshepo <rocboyt@gmail.com>
2019-06-15 18:56:38 +0200

    Cofigued model

--- app/src/main/java/co/za/dvt/airportapp/models/NearbyAirportsModel.java
+++ app/src/main/java/co/za/dvt/airportapp/models/NearbyAirportsModel.java
@@ -1,8 +1,12 @@
 package co.za.dvt.airportapp.models;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class NearbyAirportsModel {
+    @SerializedName("airports")
+    @Expose
     private List<AirportModel> airports;
 
     public List<AirportModel> getAirports() {
